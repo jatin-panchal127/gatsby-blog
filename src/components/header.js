1e7b43910ade86e471632d44d2bafc86671246fe
jatin-panchal127 <jatin.hp482k1@gmail.com>
2023-02-19 19:00:26 +0530

    gatsby blog push

---
+++ src/components/header.js
@@ -0,0 +1,38 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      margin: `0 auto`,
+      padding: `var(--space-4) var(--size-gutter)`,
+      display: `flex`,
+      alignItems: `center`,
+      justifyContent: `space-between`,
+    }}
+  >
+    <Link
+      to="/"
+      style={{
+        fontSize: `var(--font-sm)`,
+        textDecoration: `none`,
+        border: `1px solid blue`,
+        borderRadius: `15px`,
+        padding: `10px`,
+      }}
+    >
+      JixxyBlog
+    </Link>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,59 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+import styled from "styled-components"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import { Helmet } from "react-helmet"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+export default ({ data }) => (
+  <Layout>
+    <Helmet>
+      <title>Jixxy Blog - Get the Latest news before others</title>
+    </Helmet>
+    <Seo title="Home" />
+    <div>
+      <h1>Good morning!</h1>
+      {
+        data.allMarkdownRemark.edges.map(({ node }) => {
+          return <div key={node.id}>
+            <BlogLink to={node.fields.slug}><BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle></BlogLink>
+            <p>{node.excerpt}</p>
+          </div>
+        })
+      }
+    </div>
+  </Layout >
+)
+
+export const Head = () => <Seo title="Home" />
+
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          fields{
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
